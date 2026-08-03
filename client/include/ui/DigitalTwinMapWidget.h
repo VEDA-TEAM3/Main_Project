5e3c2e8119aac0d5788a660598cdee5e2abad99d
github jung_wui <sonbal7762@gmail.com>
2026-08-03 10:59:29 +0900

    feat: [TP-171] 탑뷰 수정

--- client/include/ui/DigitalTwinMapWidget.h
+++ client/include/ui/DigitalTwinMapWidget.h
@@ -63,6 +63,7 @@ private:
         QGraphicsPixmapItem* marker = nullptr;
         QGraphicsSimpleTextItem* label = nullptr;
         QGraphicsPathItem* trail = nullptr;
+        QGraphicsPathItem* trailArrow = nullptr;
         QVector<QPointF> recentPositions;
         DigitalTwinRiskLevel visibleRiskLevel = DigitalTwinRiskLevel::Normal;
     };
@@ -85,6 +86,7 @@ private:
     void rebuildVisualItemIndexes();
     QPointF scenePointFromNormalized(const QPointF& normalizedPosition) const;
     QPainterPath createTrailPath(const QVector<QPointF>& positions) const;
+    QPainterPath createTrailArrowPath(const QVector<QPointF>& positions, double tipOffset) const;
     void fitMapInView();
     std::optional<VideoFrameTimestamp> representativeVideoTimestamp(qint64 currentTimeMsec) const;
 
